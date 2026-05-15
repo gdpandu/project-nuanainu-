d9a4844dab99ba0fcfd25d18972833dc06f329aa
gdpandu <150568208+gdpandu@users.noreply.github.com>
2026-05-15 23:39:54 +0800

    Delete lib directory

--- lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://suzxhdlxzxgivkujbcml.supabase.co";
-const supabaseKey = "sb_publishable_h0UdqFLxAOfuXV4xfQEwiw_NtUi9cse";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
